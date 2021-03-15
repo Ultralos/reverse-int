5105d10fb2f6d171ed3a597c4d5d173bf1d418da
Yusuf Giyaziddinov <ygiyaziddinov@gmail.com>
2021-03-15 13:39:34 +0500

    feat: Test Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    n = n + "";
+
+    let reverse = n.substr(0, 1) === '-' ? n.split("").slice(1).reverse() : n.split("").reverse()
+   
+    for(i=0; i< reverse.length; i++) {
+        if(reverse[i] !== '0' ){
+          return reverse.join("");
+        }else{
+         reverse.shift()
+        }
+        }
+        return reverse.join("")
+      
 }
